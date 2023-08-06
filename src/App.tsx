9bbef07a81e6d59b2e7b8e37d948000b70ba18a6
Xepobopa <ismartsdn4477@gmail.com>
2023-08-06 22:39:56 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,50 @@
 import React, {useEffect} from 'react';
 import {useTelegram} from "./hooks/useTelegram";
-import Dropdown from "react-bootstrap/Dropdown";
 import 'bootstrap/dist/css/bootstrap.css';
+import {Col, Container, Form, FormControl, FormGroup, FormLabel, FormSelect, Row, Stack} from "react-bootstrap";
 
 function App() {
-    const {tg, } = useTelegram();
+    const {tg,} = useTelegram();
 
     useEffect(() => {
         tg.ready();
     }, [tg])
 
     return (
-        <div className="App">
+        <Container className={'my-4'}>
             {/*<Header />*/}
             {/*<button onClick={onToggleButton}>Toggle</button>*/}
-            <Dropdown className={'border-0'}>
-                <Dropdown.Toggle>Предмет</Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item href={"#"}>test</Dropdown.Item>
-                    <Dropdown.Item href={"#"}>test1</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-        </div>
+            <Form>
+                <Stack gap={3}>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="title">
+                                <Form.Label>Добавть домашнее задание</Form.Label>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <FormGroup className={'col-md-6'} controlId={'subject'}>
+                                <FormLabel>Предмет</FormLabel>
+                                <FormSelect>
+                                    <option>-</option>
+                                    <option value="1">Математика</option>
+                                    <option value="2">Укр Мова</option>
+                                </FormSelect>
+                            </FormGroup>
+                        </Col>
+                        <Col>
+                            <FormGroup className={'col-md-6'} controlId={'date'}>
+                                <FormLabel>Дата, когда задание было задано</FormLabel>
+                                <FormControl required name={'date'} type={'date'}
+                                             max={new Date().toISOString().split("T")[0]}/>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                </Stack>
+            </Form>
+        </Container>
     );
 }
 
