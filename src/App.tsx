b0e50be44acbf9cee19f7e32fbbc4f2b7c04e014
Xepobopa <ismartsdn4477@gmail.com>
2023-08-07 10:55:28 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,84 @@
-import React, {useEffect} from 'react';
+import React, {ChangeEvent, FormEvent, SyntheticEvent, useCallback, useEffect, useState} from 'react';
 import {useTelegram} from "./hooks/useTelegram";
 import 'bootstrap/dist/css/bootstrap.css';
 import {Col, Container, Form, FormControl, FormGroup, FormLabel, FormSelect, Row, Stack} from "react-bootstrap";
 
 function App() {
-    const {tg,} = useTelegram();
+    const {tg, onToggleButton} = useTelegram();
+    const [subject, setSubject] = useState<string>(null!);
+    const [date, setDate] = useState<string>(null!);
+    const [title, setTitle] = useState<string>(null!);
+    const [images, setImages] = useState<FileList | null>(null);
 
     useEffect(() => {
         tg.ready();
     }, [tg])
 
+    useEffect(() => {
+        tg.WebApp.onEvent('mainButtonClicked', onSendData);
+        return () => {
+            tg.WebApp.offEvent('mainButtonClicked', onSendData);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (images && date && title && images)
+            onToggleButton();
+        else
+            onToggleButton();
+    }, [subject, date, title, images, onToggleButton])
+
+    const onSendData = useCallback(() => {
+       const data = {
+           subject,
+           date,
+           title,
+           images
+       }
+       tg.sendData(JSON.stringify(data));
+    }, [date, images, subject, tg, title]);
+
+    const handleSelectSubject = (e: SyntheticEvent<HTMLSelectElement>) => {
+        e.preventDefault();
+        setSubject(e.currentTarget.value);
+    }
+
+    const handleSelectDate = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setDate(e.currentTarget.value);
+    }
+
+    const handleChangeFiles = async (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        console.log(e.target.files)
+        setImages(e.target.files);
+    }
+
+    const handleChangeTitle = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        e.preventDefault();
+        setTitle(e.currentTarget.value);
+    }
+
     return (
         <Container className={'my-4'}>
-            {/*<Header />*/}
             {/*<button onClick={onToggleButton}>Toggle</button>*/}
             <Form>
                 <Stack gap={3}>
                     <Row>
                         <Col>
-                            <Form.Group controlId="title">
-                                <Form.Label>Добавть домашнее задание</Form.Label>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row style={{'verticalAlign': 'bottom'}}>
-                        <Col>
                             <FormGroup className={'col-md-6'} controlId={'subject'}>
                                 <FormLabel>Предмет</FormLabel>
-                                <FormSelect>
+                                <FormSelect onChange={handleSelectSubject}>
                                     <option>-</option>
-                                    <option value="1">Математика</option>
-                                    <option value="2">Укр Мова</option>
+                                    <option>Математика</option>
+                                    <option>Укр Мова</option>
                                 </FormSelect>
                             </FormGroup>
                         </Col>
                         <Col>
                             <FormGroup className={'col-md-6'} controlId={'date'}>
                                 <FormLabel>Когда задали дз</FormLabel>
-                                <FormControl required name={'date'} type={'date'}
+                                <FormControl required name={'date'} type={'date'} onSelect={handleSelectDate}
                                              max={new Date().toISOString().split("T")[0]}/>
                             </FormGroup>
                         </Col>
@@ -45,13 +86,13 @@ function App() {
                     <Row>
                         <Form.Group controlId="markdown">
                             <Form.Label>Название задания</Form.Label>
-                            <Form.Control required as="textarea" name="body" rows={2}/>
+                            <Form.Control required as="textarea" name="body" rows={2} onChange={handleChangeTitle}/>
                         </Form.Group>
                     </Row>
                     <Row>
                         <Form.Group controlId="files">
                             <Form.Label>Фото</Form.Label>
-                            <Form.Control required type={'file'} name="file" multiple/>
+                            <Form.Control required type={'file'} name="file" onChange={handleChangeFiles} multiple/>
                         </Form.Group>
                     </Row>
                 </Stack>
